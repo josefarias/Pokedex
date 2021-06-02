5374446b7d2886d2df564a0794a135ed7d0ce43a
Jose Farias <jose@farias.mx>
2021-06-01 21:17:12 -0500

    Better explain enforceDataIntegrity

--- src/utils/DataIntegrityHelpers.ts
+++ src/utils/DataIntegrityHelpers.ts
@@ -2,9 +2,9 @@
 import { camelize } from './StringHelpers'
 
 // NOTE: Another way to do this would be to give each property default values.
-//  Using this pattern assumes we're creating a `defaultValues: ServerInterface`
-//  constant for all models. That way, TS will call us out if we forget to
-//  set a default value.
+//  Using this pattern allows us to set a `defaultValues: ServerInterface`
+//  constant for all models. Which gives us a way to use the same defaults for
+//  class properties and empty versions of our models.
 export function enforceDataIntegrity(obj: Object, defaultValues: Object) {
   Object.keys(defaultValues).forEach((key: string) => {
     if (obj[camelize(key)]) return
